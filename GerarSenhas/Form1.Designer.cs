d52b1177c30f2a89d6af759c4363bd02df0ebcff
Paola Garbato <paolagarbato@gmail.com>
2020-09-14 21:46:04 -0300

    Finalizando código

--- GerarSenhas/Form1.Designer.cs
+++ GerarSenhas/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCaracteres = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(430, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(127, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gerador de Senhas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(82, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Quantidade de caracteres:";
+            // 
+            // txtCaracteres
+            // 
+            this.txtCaracteres.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtCaracteres.Location = new System.Drawing.Point(312, 119);
+            this.txtCaracteres.Name = "txtCaracteres";
+            this.txtCaracteres.Size = new System.Drawing.Size(70, 23);
+            this.txtCaracteres.TabIndex = 3;
+            this.txtCaracteres.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCaracteres_KeyPress);
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSenha.Location = new System.Drawing.Point(178, 240);
+            this.lblSenha.Margin = new System.Windows.Forms.Padding(0);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(0, 35);
+            this.lblSenha.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Snow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 14F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(200, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GERAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(479, 294);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtCaracteres);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCaracteres;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GerarSenhas/Form1.cs
+++ GerarSenhas/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,60 @@ namespace GerarSenhas
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string CreatePassword()
+        {
+            int qtd = int.Parse(txtCaracteres.Text);
+            string password = string.Empty;
+           
+
+            for (int i = 1; i <= qtd; i++)
+            {
+                Random random = new Random();
+                int code = Convert.ToInt32(random.Next(48, 122).ToString());
+
+                if (code >= 48 && code <= 57 || code >= 97 && code <= 122)
+                {
+                    string caractere = ((char)code).ToString();
+                    if (!password.Contains(caractere))
+                    {
+                        password += caractere;
+                    } else
+                    {
+                        i--;
+                    }
+                } else
+                {
+                    i--;
+                }
+            }
+            return password.ToUpper();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // lblSenha.Margin
+            //  Margins margin = new Margins(0, 0, 0, 0);
+            lblSenha.Text = CreatePassword();
+        }
+
+        private void txtCaracteres_KeyPress(object sender, KeyPressEventArgs e)
+        {
+             if(!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
         }
     }
 }
